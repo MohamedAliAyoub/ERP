c6c0f6dfe9032374ebc3ac06f72a9b48f403806f
MohamedAliAyoub <mohamedali163163@gmail.com>
2023-03-03 23:21:19 +0200

    change employee dashboard

--- app/Http/Controllers/Member/MemberDashboardController.php
+++ app/Http/Controllers/Member/MemberDashboardController.php
@@ -4,14 +4,19 @@ namespace App\Http\Controllers\Member;
 
 use App\Attendance;
 use App\AttendanceSetting;
+use App\EmployeeDetails;
+use App\EmployeeDocs;
 use App\Holiday;
 use App\LanguageSetting;
+use App\Leave;
+use App\LeaveType;
 use App\Notice;
 use App\Project;
 use App\ProjectActivity;
 use App\ProjectTimeLog;
 use App\Task;
 use App\TaskboardColumn;
+use App\User;
 use App\UserActivity;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
@@ -29,103 +34,47 @@ class MemberDashboardController extends MemberBaseController
 
     public function index()
     {
-        $this->languageSettings = LanguageSetting::where('status', 'enabled')->get();
-        // $this->timer = ProjectTimeLog::memberActiveTimer($this->user->id);
-        // Getting Attendance setting data
-        $this->attendanceSettings = AttendanceSetting::first();
-        //Getting Maximum Check-ins in a day
-        $this->maxAttandenceInDay = $this->attendanceSettings->clockin_in_day;
 
-        $this->totalProjects = Project::select('projects.id');
-
-
-        if (!$this->user->cans('view_projects')) {
-            $this->totalProjects->join('project_members', 'project_members.project_id', '=', 'projects.id');
-            $this->totalProjects = $this->totalProjects->where('project_members.user_id', '=', $this->user->id);
+        $id = auth()->id();
+        $this->employee = User::with(['employeeDetail', 'employeeDetail.designation', 'employeeDetail.department', 'leaveTypes'])->withoutGlobalScope('active')->findOrFail($id);
+        $this->employeeDetail = EmployeeDetails::where('user_id', '=', $this->employee->id)->first();
+        $this->employeeDocs = EmployeeDocs::where('user_id', '=', $this->employee->id)->get();
+        if (!is_null($this->employeeDetail)) {
+            $this->employeeDetail = $this->employeeDetail->withCustomFields();
+            $this->fields = $this->employeeDetail->getCustomFieldGroupsWithFields()->fields;
         }
 
-        $taskBoardColumn = TaskboardColumn::all();
+        $completedTaskColumn = TaskboardColumn::where('slug', 'completed')->first();
 
-        $incompletedTaskColumn = $taskBoardColumn->filter(function ($value, $key) {
-            return $value->slug == 'incomplete';
-        })->first();
+        $this->taskCompleted = Task::join('task_users', 'task_users.task_id', '=', 'tasks.id')
+            ->where('task_users.user_id', $id)
+            ->where('tasks.board_column_id', $completedTaskColumn->id)
+            ->count();
 
-        $completedTaskColumn = $taskBoardColumn->filter(function ($value, $key) {
-            return $value->slug == 'completed';
-        })->first();
+        $hoursLogged = ProjectTimeLog::where('user_id', $id)->sum('total_minutes');
 
-        $this->totalProjects = $this->totalProjects->count();
-        $this->counts = DB::table('users')
+        $timeLog = intdiv($hoursLogged, 60) . ' hrs ';
 
-            ->select(
-                DB::raw('(select IFNULL(sum(project_time_logs.total_minutes),0) from `project_time_logs` where user_id = ' . $this->user->id . ') as totalHoursLogged '),
-                DB::raw('(select count(tasks.id) from `tasks` inner join task_users on task_users.task_id=tasks.id where tasks.board_column_id=' . $completedTaskColumn->id . ' and task_users.user_id = ' . $this->user->id . ') as totalCompletedTasks'),
-                DB::raw('(select count(tasks.id) from `tasks` inner join task_users on task_users.task_id=tasks.id where tasks.board_column_id=' . $incompletedTaskColumn->id . ' and task_users.user_id = ' . $this->user->id . ') as totalPendingTasks')
-            )
-            ->first();
-
-        $timeLog = intdiv($this->counts->totalHoursLogged, 60) . ' ' . __('modules.hrs'). ' ';
-
-        if (($this->counts->totalHoursLogged % 60) > 0) {
-            $timeLog .= ($this->counts->totalHoursLogged % 60) . ' ' . __('modules.mins');
+        if (($hoursLogged % 60) > 0) {
+            $timeLog .= ($hoursLogged % 60) . ' mins';
         }
 
-        $this->counts->totalHoursLogged = $timeLog;
+        $this->hoursLogged = $timeLog;
 
-        $this->projectActivities = ProjectActivity::join('projects', 'projects.id', '=', 'project_activity.project_id')
+        $this->activities = UserActivity::where('user_id', $id)->orderBy('id', 'desc')->get();
+        $this->projects = Project::select('projects.id', 'projects.project_name', 'projects.deadline', 'projects.completion_percent')
             ->join('project_members', 'project_members.project_id', '=', 'projects.id')
-            ->where('project_members.user_id', '=', $this->user->id)
-            ->whereNull('projects.deleted_at')
-            ->select('projects.project_name', 'project_activity.created_at', 'project_activity.activity', 'project_activity.project_id')
-            ->limit(15)->orderBy('project_activity.id', 'desc')->get();
-
-        $this->userActivities = UserActivity::limit(15)->orderBy('id', 'desc')->where('user_id', $this->user->id)->get();
-
-        $this->pendingTasks = Task::join('task_users', 'task_users.task_id', '=', 'tasks.id')
-            ->where('tasks.board_column_id', $incompletedTaskColumn->id)
-            ->where(DB::raw('DATE(due_date)'), '<=', Carbon::today()->format('Y-m-d'))
-            ->where('task_users.user_id', $this->user->id)
-            ->select('tasks.*')
-            ->groupBy('tasks.id')
+            ->where('project_members.user_id', '=', $id)
             ->get();
+        $this->leaves = Leave::byUser($id);
+        $this->leavesCount = Leave::byUserCount($id);
+
+        $this->leaveTypes = LeaveType::byUser($id);
+        $this->allowedLeaves = $this->employee->leaveTypes->sum('no_of_leaves');
+        $this->employeeLeavesQuota = $this->employee->leaveTypes;
 
 
-        // Getting Current Clock-in if exist
-        $this->currenntClockIn = Attendance::where(DB::raw('DATE(clock_in_time)'), Carbon::today()->format('Y-m-d'))
-            ->where('user_id', $this->user->id)->whereNull('clock_out_time')->first();
-        // Getting Today's Total Check-ins
-        $this->todayTotalClockin = Attendance::where(DB::raw('DATE(clock_in_time)'), Carbon::today()->format('Y-m-d'))
-            ->where('user_id', $this->user->id)->where(DB::raw('DATE(clock_out_time)'), Carbon::today()->format('Y-m-d'))->count();
-
-        $currentDate = Carbon::now()->format('Y-m-d');
-
-        // Check Holiday by date
-        $this->checkTodayHoliday = Holiday::where('date', $currentDate)->first();
-
-        //check office time passed
-        $officeEndTime = Carbon::createFromFormat('H:i:s', $this->attendanceSettings->office_end_time, $this->global->timezone)->timestamp;
-        $currentTime = Carbon::now()->timezone($this->global->timezone)->timestamp;
-        if ($officeEndTime < $currentTime) {
-            $this->noClockIn = true;
-        }
-
-
-        if ($this->user->cans('view_timelogs') && in_array('timelogs', $this->user->modules)) {
-
-            $this->activeTimerCount = ProjectTimeLog::with('user', 'project', 'task')
-                ->whereNull('end_time')
-                ->join('users', 'users.id', '=', 'project_time_logs.user_id');
-
-            $this->activeTimerCount = $this->activeTimerCount
-                ->select('project_time_logs.*', 'users.name')
-                ->count();
-        }
-
-        if ($this->user->cans('view_notice') && in_array('notices', $this->user->modules)) {
-            $this->notices = Notice::limit(8)->latest()->get();
-        }
-
-        return view('member.dashboard.index', $this->data);
+        return view('member.dashboard.index-info', $this->data);
     }
 
 }
